737ddbd5f9c9e3cbc9b3273f520eafde6ffae5ca
Brian Harrison <brianharrison@rogers.com>
2016-07-06 18:53:35 -0400

    Got the switch to work..nice

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+$(document).on('ready', function() {
+
+    $("button").click(function(){
+        $(".light").toggleClass("dark");
+        $(".status").toggle();
+        $(".on").toggleClass("off");
+    });
+});
 
 
+//     if($('button').hasClass("on")) {
+//       $("body").addClass("dark");
+//     }
+//     else {
+//       $("body").removeClass("dark");
+//     });
+// });
